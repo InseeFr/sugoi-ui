4511247c3daf93b377bf8299ff326cff737ce93f
CChemin <cecile.chemin@insee.fr>
2020-12-18 13:49:33 +0100

    refactor roles matching with regex
    Signed-off-by: CChemin <cecile.chemin@insee.fr>

--- src/utils/roles.ts
+++ src/utils/roles.ts
@@ -1,42 +1,10 @@
-export const isReader = (roles: string[], readerRegexName: string) => {
-	const regex = new RegExp(readerRegexName);
-	let readerDomain: any[] = [];
-	if (roles) {
-		readerDomain = roles
-			.filter((role: string) => role.match(regex))
-			.map((role: string) => {
-				const found = role.match(regex);
-				return found ? found[1] : null;
-			});
-	}
-	return [
-		readerDomain.length ? readerDomain.length > 0 : false,
-		readerDomain,
-	];
-};
+export const getRegexDomains = (roles: string[], regex: string): string[] =>
+	roles
+		.filter((role: string) => role.match(RegExp(regex)))
+		.map((role: string): string => {
+			const matchingRoles = role.match(RegExp(regex));
+			return matchingRoles ? matchingRoles[1] : '';
+		});
 
-export const isAdministrator = (roles: string[], adminName: string) => {
-	let t: any = [];
-	if (roles) {
-		t = roles.filter((role: string) => role.includes(adminName)).length;
-	}
-	return t ? t > 0 : false;
-};
-
-export const isWriter = (roles: string[], writerRegexName: string) => {
-	const regex = new RegExp(writerRegexName);
-	let writeDomain: any[] = [];
-	if (roles) {
-		writeDomain = roles
-			.filter((role: string) => role.match(regex))
-			.map((role: string) => {
-				const found = role.match(regex);
-				return found ? found[1] : null;
-			});
-	}
-	return [writeDomain.length ? writeDomain.length > 0 : false, writeDomain];
-};
-
-export const getRoles = (tokenParsed: any | undefined) => {
-	return null;
-};
+export const isAdministrator = (roles: string[], adminName: string): boolean =>
+	roles.includes(adminName);
