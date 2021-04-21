2887b3aaf8f7432f797f7a7f481af8f65778bcdf
Donatien Eneman <donatien.eneman@insee.fr>
2021-04-21 11:10:30 +0200

    fix 108-Manage-appmanager-permissions
    Signed-off-by: Donatien Eneman <donatien.eneman@insee.fr>

--- src/components/commons/sider/sider-body.tsx
+++ src/components/commons/sider/sider-body.tsx
@@ -24,6 +24,8 @@ import { RootState } from '../../../configuration/store-configuration';
 import { useGetRealms } from '../../../hooks/realm/useGetRealms';
 import { saveRealms } from '../../../redux/actions/app';
 import GrainIcon from '@material-ui/icons/Grain';
+import { useSnackbar } from 'notistack';
+
 const useStyle = makeStyles((theme: Theme) =>
 	createStyles({
 		nested: {
@@ -43,6 +45,8 @@ const SiderBody = () => {
 	const { t } = useTranslation();
 	const user = useSelector((state: RootState) => state.user);
 	const [realmSelected, setRealmSelected] = useState<string | undefined>();
+	const { enqueueSnackbar } = useSnackbar();
+
 	const [userStorageSelected, setStorageSelected] = useState<
 		string | undefined
 	>();
@@ -90,6 +94,13 @@ const SiderBody = () => {
 				}
 			} else {
 				push('/');
+				enqueueSnackbar("Vous n'avez pas à être la", {
+					variant: 'error',
+					anchorOrigin: {
+						vertical: 'top',
+						horizontal: 'center',
+					},
+				});
 			}
 		} else {
 			setRealmSelected(undefined);
@@ -114,204 +125,7 @@ const SiderBody = () => {
 					<ListItemText primary={t('sider.home')} />
 				</ListItem>
 				<Divider />
-				{user.role.isAdmin ||
-				user.role.isReader ||
-				user.role.isWriter ? (
-					<List
-						component="nav"
-						aria-labelledby="nested-list-subheader"
-						subheader={
-							<ListSubheader
-								component="div"
-								id="nested-list-subheader"
-							>
-								{t('sider.manage.title')}
-							</ListSubheader>
-						}
-					>
-						<ListItem className={classes.nested}>
-							<Autocomplete
-								id="realm choice"
-								options={realms.map(
-									(realm) => realm.name,
-								)}
-								style={{ width: 300 }}
-								value={realmSelected || null}
-								onChange={(
-									_event: any,
-									newRealm: string | null,
-								) => {
-									setRealmSelected(
-										newRealm ||
-											undefined,
-									);
-									setStorageSelected(
-										undefined,
-									);
-									push(
-										'/realm/' +
-											newRealm,
-									);
-								}}
-								renderInput={(params) => (
-									<TextField
-										{...params}
-										label="Realm"
-										variant="filled"
-									/>
-								)}
-							/>
-						</ListItem>
-						<ListItem className={classes.nestedUS}>
-							<Autocomplete
-								id="userStorage choice"
-								disabled={
-									realmSelected
-										? false
-										: true
-								}
-								options={realms
-									?.filter(
-										(realm) =>
-											realm.name ===
-											realmSelected,
-									)[0]
-									?.userStorages.map(
-										(us) => us.name,
-									)}
-								style={{ width: 300 }}
-								value={
-									userStorageSelected ||
-									null
-								}
-								onChange={(
-									_event: any,
-									newUserStorage:
-										| string
-										| null,
-								) => {
-									setStorageSelected(
-										newUserStorage ||
-											undefined,
-									);
-									push(
-										'/realm/' +
-											realmSelected +
-											'/us/' +
-											newUserStorage,
-									);
-								}}
-								renderInput={(params) => (
-									<TextField
-										{...params}
-										label="User Storage"
-										variant="filled"
-									/>
-								)}
-							/>
-						</ListItem>
-						<ListItem
-							button
-							key="search_users"
-							disabled={
-								realmSelected ? false : true
-							}
-							onClick={() =>
-								userStorageSelected
-									? push(
-											'/realm/' +
-												realmSelected +
-												'/us/' +
-												userStorageSelected +
-												'/users',
-									  )
-									: push(
-											'/realm/' +
-												realmSelected +
-												'/users',
-									  )
-							}
-							className={classes.nestedUS}
-							selected={location.pathname.includes(
-								'/users',
-							)}
-						>
-							<ListItemIcon>
-								<PersonIcon />
-							</ListItemIcon>
-							<ListItemText
-								primary={t(
-									'sider.manage.users',
-								)}
-							/>
-						</ListItem>
-						<ListItem
-							button
-							key="search_organizations"
-							disabled={
-								realmSelected ? false : true
-							}
-							onClick={() =>
-								userStorageSelected
-									? push(
-											'/realm/' +
-												realmSelected +
-												'/us/' +
-												userStorageSelected +
-												'/organizations',
-									  )
-									: push(
-											'/realm/' +
-												realmSelected +
-												'/organizations',
-									  )
-							}
-							className={classes.nestedUS}
-							selected={location.pathname.includes(
-								'/organizations',
-							)}
-						>
-							<ListItemIcon>
-								<BusinessIcon />
-							</ListItemIcon>
-							<ListItemText
-								primary={t(
-									'sider.manage.organizations',
-								)}
-							/>
-						</ListItem>
-						<ListItem
-							button
-							key="search_application"
-							disabled={
-								realmSelected ? false : true
-							}
-							onClick={() =>
-								push(
-									'/realm/' +
-										realmSelected +
-										'/applications',
-								)
-							}
-							className={classes.nested}
-							selected={location.pathname.includes(
-								'/applications',
-							)}
-						>
-							<ListItemIcon>
-								<GrainIcon />
-							</ListItemIcon>
-							<ListItemText
-								primary={t(
-									'sider.manage.applications',
-								)}
-							/>
-						</ListItem>
-					</List>
-				) : null}
-			</List>
-			<Divider />
-			{user.role.isAdmin ? (
+
 				<List
 					component="nav"
 					aria-labelledby="nested-list-subheader"
@@ -320,30 +134,204 @@ const SiderBody = () => {
 							component="div"
 							id="nested-list-subheader"
 						>
-							{t('sider.configuration.title')}
+							{t('sider.manage.title')}
 						</ListSubheader>
 					}
 				>
+					<ListItem className={classes.nested}>
+						<Autocomplete
+							id="realm choice"
+							options={realms.map(
+								(realm) => realm.name,
+							)}
+							style={{ width: 300 }}
+							value={realmSelected || null}
+							onChange={(
+								_event: any,
+								newRealm: string | null,
+							) => {
+								setRealmSelected(
+									newRealm || undefined,
+								);
+								setStorageSelected(undefined);
+								if (newRealm) {
+									push(
+										'/realm/' +
+											newRealm,
+									);
+								} else {
+									push('/');
+								}
+							}}
+							renderInput={(params) => (
+								<TextField
+									{...params}
+									label="Realm"
+									variant="filled"
+								/>
+							)}
+						/>
+					</ListItem>
+					<ListItem className={classes.nestedUS}>
+						<Autocomplete
+							id="userStorage choice"
+							disabled={
+								realmSelected ? false : true
+							}
+							options={realms
+								?.filter(
+									(realm) =>
+										realm.name ===
+										realmSelected,
+								)[0]
+								?.userStorages.map(
+									(us) => us.name,
+								)}
+							style={{ width: 300 }}
+							value={userStorageSelected || null}
+							onChange={(
+								_event: any,
+								newUserStorage: string | null,
+							) => {
+								setStorageSelected(
+									newUserStorage ||
+										undefined,
+								);
+								push(
+									'/realm/' +
+										realmSelected +
+										'/us/' +
+										newUserStorage,
+								);
+							}}
+							renderInput={(params) => (
+								<TextField
+									{...params}
+									label="User Storage"
+									variant="filled"
+								/>
+							)}
+						/>
+					</ListItem>
 					<ListItem
 						button
-						key="parametres"
-						onClick={() => push('/settings')}
-						className={classes.nested}
+						key="search_users"
+						disabled={realmSelected ? false : true}
+						onClick={() =>
+							userStorageSelected
+								? push(
+										'/realm/' +
+											realmSelected +
+											'/us/' +
+											userStorageSelected +
+											'/users',
+								  )
+								: push(
+										'/realm/' +
+											realmSelected +
+											'/users',
+								  )
+						}
+						className={classes.nestedUS}
 						selected={location.pathname.includes(
-							'/settings',
+							'/users',
 						)}
 					>
 						<ListItemIcon>
-							<SettingsIcon />
+							<PersonIcon />
+						</ListItemIcon>
+						<ListItemText
+							primary={t('sider.manage.users')}
+						/>
+					</ListItem>
+					<ListItem
+						button
+						key="search_organizations"
+						disabled={realmSelected ? false : true}
+						onClick={() =>
+							userStorageSelected
+								? push(
+										'/realm/' +
+											realmSelected +
+											'/us/' +
+											userStorageSelected +
+											'/organizations',
+								  )
+								: push(
+										'/realm/' +
+											realmSelected +
+											'/organizations',
+								  )
+						}
+						className={classes.nestedUS}
+						selected={location.pathname.includes(
+							'/organizations',
+						)}
+					>
+						<ListItemIcon>
+							<BusinessIcon />
 						</ListItemIcon>
 						<ListItemText
 							primary={t(
-								'sider.configuration.settings',
+								'sider.manage.organizations',
+							)}
+						/>
+					</ListItem>
+					<ListItem
+						button
+						key="search_application"
+						disabled={realmSelected ? false : true}
+						onClick={() =>
+							push(
+								'/realm/' +
+									realmSelected +
+									'/applications',
+							)
+						}
+						className={classes.nested}
+						selected={location.pathname.includes(
+							'/applications',
+						)}
+					>
+						<ListItemIcon>
+							<GrainIcon />
+						</ListItemIcon>
+						<ListItemText
+							primary={t(
+								'sider.manage.applications',
 							)}
 						/>
 					</ListItem>
 				</List>
-			) : null}
+			</List>
+			<Divider />
+			<List
+				component="nav"
+				aria-labelledby="nested-list-subheader"
+				subheader={
+					<ListSubheader
+						component="div"
+						id="nested-list-subheader"
+					>
+						{t('sider.configuration.title')}
+					</ListSubheader>
+				}
+			>
+				<ListItem
+					button
+					key="parametres"
+					onClick={() => push('/settings')}
+					className={classes.nested}
+					selected={location.pathname.includes('/settings')}
+				>
+					<ListItemIcon>
+						<SettingsIcon />
+					</ListItemIcon>
+					<ListItemText
+						primary={t('sider.configuration.settings')}
+					/>
+				</ListItem>
+			</List>
 			<Divider />
 		</>
 	);
