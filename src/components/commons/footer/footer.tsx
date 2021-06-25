652c3fe0e2172976d10c9df5c8d4b65753de8e7e
Donatien Eneman <donatien.eneman@insee.fr>
2021-06-25 11:29:15 +0200

    [ENH] :sparkles: Allow to add version and commit sha on bottom page by configuration.json
    Signed-off-by: Donatien Eneman <donatien.eneman@insee.fr>

--- src/components/commons/footer/footer.tsx
+++ src/components/commons/footer/footer.tsx
@@ -1,9 +1,12 @@
 import { Grid, Typography } from '@material-ui/core';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { useConfig } from '../../../hooks/technics/useConfigFile';
 
 const Footer = () => {
 	const { t } = useTranslation();
+	const version = useConfig('app_version');
+	const commit_sha = useConfig('app_last_commit_sha');
 	return (
 		<>
 			<Grid
@@ -18,8 +21,8 @@ const Footer = () => {
 						color="textSecondary"
 						align="center"
 					>
-						{t('commons.footer.version.text')}:
-						{process.env.REACT_APP_UI_VERSION}
+						{t('commons.footer.version.text')} :{' '}
+						{version}
 					</Typography>
 				</Grid>
 				<Grid item xs={12}>
@@ -28,8 +31,8 @@ const Footer = () => {
 						color="textSecondary"
 						align="center"
 					>
-						{t('commons.footer.commit.text')}:
-						{process.env.REACT_APP_UI_COMMIT_SHA}
+						{t('commons.footer.commit.text')} :{' '}
+						{commit_sha}
 					</Typography>
 				</Grid>
 			</Grid>
