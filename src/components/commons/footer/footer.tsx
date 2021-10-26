8bd3786a283751b51a07d70e7ba1200a11d28832
CChemin <57364244+CChemin@users.noreply.github.com>
2021-10-26 11:30:40 +0200

    [BUG] :bug: Add version at build time (#253) (#254)
    Signed-off-by: CChemin <cecile.chemin@insee.fr>

--- src/components/commons/footer/footer.tsx
+++ src/components/commons/footer/footer.tsx
@@ -1,5 +1,4 @@
 import { Grid, Typography } from '@material-ui/core';
-import React from 'react';
 import { useTranslation } from 'react-i18next';
 
 const Footer = () => {
@@ -12,18 +11,8 @@ const Footer = () => {
 					color="textSecondary"
 					align="center"
 				>
-					{t('commons.footer.version.text')}:
+					{t('commons.footer.version.text')}:{' '}
 					{process.env.REACT_APP_UI_VERSION}
-				</Typography>
-			</Grid>
-			<Grid item xs={12}>
-				<Typography
-					variant="body2"
-					color="textSecondary"
-					align="center"
-				>
-					{t('commons.footer.commit.text')}:
-					{process.env.REACT_APP_UI_COMMIT_SHA}
 				</Typography>
 			</Grid>
 		</Grid>
