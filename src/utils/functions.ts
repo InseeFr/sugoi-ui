09b534230cb9769eacd8e8fab522281668615038
Donatien Eneman <donatien.eneman@insee.fr>
2021-11-09 09:21:15 +0100

    [BUG] :bug: fix error displayed when no app branch configured on realm home
    Signed-off-by: Donatien Eneman <donatien.eneman@insee.fr>

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -23,5 +23,9 @@ export const realmHasOrganization: (
 };
 
 export const realmHasApplication = (realm: Realm) => {
-	return realm?.name !== undefined && realm?.appSource !== undefined;
+	return (
+		realm?.name !== undefined &&
+		realm?.appSource !== undefined &&
+		realm?.appSource !== null
+	);
 };
