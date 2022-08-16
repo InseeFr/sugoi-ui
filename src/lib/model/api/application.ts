569599a11c09bd54fd45731d1f7593b12a7ed685
Sommer Le <pierrick.le-sommer@insee.fr>
2022-08-16 13:43:20 +0200

    [ENH] Details application should allow to see and manage Application attributes
    Signed-off-by: Sommer Le <pierrick.le-sommer@insee.fr>

--- src/lib/model/api/application.ts
+++ src/lib/model/api/application.ts
@@ -1,9 +1,11 @@
 import { Group } from './group';
+import AttributesApplication from './attributesApplication';
 
 export interface Application {
 	name: string;
 	owner?: string;
 	groups: Group[];
+	attributes?: AttributesApplication;
 }
 
 export default Application;
