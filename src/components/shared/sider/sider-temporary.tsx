106f4519e2903e9985f48c53a70567cf365eb93e
Clément Dufaure <clement.dufaure@insee.fr>
2026-08-07 16:17:08 +0200

    [CLEAN] :recycle: remove deprecated @mui/styles
    Signed-off-by: Clément Dufaure <clement.dufaure@insee.fr>

--- src/components/shared/sider/sider-temporary.tsx
+++ src/components/shared/sider/sider-temporary.tsx
@@ -1,6 +1,4 @@
 import { Drawer, useTheme } from '@mui/material';
-import createStyles from '@mui/styles/createStyles';
-import makeStyles from '@mui/styles/makeStyles';
 import SiderBody from './sider-body';
 
 interface props {
@@ -8,17 +6,8 @@ interface props {
 	handleDrawerToggle: any;
 }
 
-const useStyles = makeStyles(() =>
-	createStyles({
-		drawerPaper: {
-			width: 240,
-		},
-	}),
-);
-
 const SiderTemporary = (props: props) => {
 	const { open, handleDrawerToggle } = props;
-	const classes = useStyles();
 	const theme = useTheme();
 
 	return (
@@ -27,8 +16,10 @@ const SiderTemporary = (props: props) => {
 			anchor={theme.direction === 'rtl' ? 'right' : 'left'}
 			open={open}
 			onClose={handleDrawerToggle}
-			classes={{
-				paper: classes.drawerPaper,
+			slotProps={{
+				paper: {
+					sx: { width: 240 },
+				},
 			}}
 			ModalProps={{
 				keepMounted: true, // Better open performance on mobile.
